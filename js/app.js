d83df9e1c3d53f3e15b8a4fab78b69faa46b244e
RayJustin <Ray.Justin@live.com>
2016-06-13 19:18:01 -0700

    Updated Finished project

--- js/app.js
+++ js/app.js
@@ -1,17 +1,15 @@
-
-
-	for (var number = 1; number <= 100; number++){
+for (var number = 1; number <= 100; number++){
 	if (number%3 == 0 && number%5 == 0) {
-		document.write("<p> fizzbuzz </p>")
+		document.write("<p> fizzbuzz </p>");
 	}
 	else if (number % 5 ==0) {
-		document.write("<p> buzz </p>")
+		document.write("<p> buzz </p>");
 	}
 	else if (number % 3 ==0) {
-		document.write("<p> fizz </p>")
+		document.write("<p> fizz </p>");
 	}
 	else {
-		document.write("<p>" + number + "</p>")
+		document.write("<p>" + number + "</p>");
 	}
-	}
+}
 
